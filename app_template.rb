5ce833b0f72c652420d884cc415a914f18754570
Dave Powers <djpowers89@gmail.com>
2014-06-11 22:43:29 -0400

    Change to single quotes

--- app_template.rb
+++ app_template.rb
@@ -1,33 +1,33 @@
-remove_file "README.rdoc"
-create_file "README.md", "TODO"
+remove_file 'README.rdoc'
+create_file 'README.md', 'TODO'
 
 gem_group :development, :test do
-  gem "pry-rails"
-  gem "rspec-rails"
-  gem "capybara"
-  gem "factory_girl_rails"
-  gem "shoulda-matchers"
-  gem "valid_attribute"
-  gem "launchy"
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
+  gem 'valid_attribute'
+  gem 'launchy'
 end
 
 gem_group :development do
-  gem "better_errors"
-  gem "binding_of_caller"
-  gem "quiet_assets"
-  gem "meta_request"
-  gem "rails-erd"
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+  gem 'meta_request'
+  gem 'rails-erd'
 end
 
-run "bundle install"
-generate "rspec:install"
+run 'bundle install'
+generate 'rspec:install'
 
-if yes? "Do you want to generate a root controller?"
-  name = ask("What should it be called?").underscore
-  generate :controller, "#{name} index"
-  route "root to: '#{name}\#index"
-  remove_file "public/index.html"
+if yes? 'Do you want to generate a root controller?'
+  name = ask('What should it be called?').underscore
+  generate :controller, '#{name} index'
+  route "root to: #{name}\#index"
+  remove_file 'public/index.html'
 end
 
 git :init
-git add: ".", commit: "-m 'Initial commit'"
+git add: '.', commit: "-m 'Initial commit'"
